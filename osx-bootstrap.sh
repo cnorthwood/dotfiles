8919e5a2ddf56443e7b19d01ae855a474dafb85b
Chris Northwood <chris.northwood@bbc.co.uk>
2016-02-08 16:24:52 +0000

    Suppress git push errors on new machines

--- osx-bootstrap.sh
+++ osx-bootstrap.sh
@@ -211,6 +211,7 @@ git config --global core.excludesfile '~/.gitignore'
 git config --global alias.newbranch '!sh -c "git checkout -b \$1 && git branch --set-upstream-to origin/master" -'
 git config --global alias.cleanup '!sh -c "git branch --merged master | grep -v \"\* master\" | xargs -n 1 git branch -d"'
 git config --global pull.rebase true
+git config --global push.default simple
 
 # Install Dropbox
 if [ ! -d "/Applications/Dropbox.app" ]; then
